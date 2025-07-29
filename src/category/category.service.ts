eff84df9f35c7cd80fecaee129290b572dae9756
ImTheOne-9 <110813494+ImTheOne-9@users.noreply.github.com>
2025-07-29 21:30:09 +0700

    config: create prisma

---
+++ src/category/category.service.ts
@@ -0,0 +1,10 @@
+// import { PrismaService } from "src/prisma/prisma.service";
+// import { CreateCategoryDto } from "./dto/create-category.dto";
+
+// export class CategoryService {
+//   constructor(private prisma: PrismaService) { }
+
+//   create(dto: CreateCategoryDto) {
+//     return this
+//   }
+// }
